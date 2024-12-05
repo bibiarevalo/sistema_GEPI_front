11b8ee1dc47ab3989025e529d1ddf82de9882852
BEATRIZ AREVALO FREITAS <beatriz_a_freitas@estudante.sesisenai.org.br>
2024-12-04 21:08:18 -0300

    feat: aeeeeeee ACABOUUUUU

---
+++ test/cypress/e2e/historico.cy.js
@@ -0,0 +1,12 @@
+describe("The Home Page", () => {
+    it("Sucessfully loads", () => {
+      cy.visit("http://localhost:5173/home");
+      cy.wait(2000);
+      cy.get("a").contains("Ir para perfil Gerência").click();
+      cy.get("#email").type("bia@gmail.com", { delay: 100 });
+      cy.get("#password").type("bia123", { delay: 100 });
+      cy.get('button[type="submit"]').click();
+      cy.wait(2000);
+
+    });
+  });
